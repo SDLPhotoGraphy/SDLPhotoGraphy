2e2bd320e0cedf0956a8ee9ea8e557b19788064c
SDLPhotoGraphy <122206595+SDLPhotoGraphy@users.noreply.github.com>
2023-03-12 13:10:42 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -76,20 +76,6 @@ function opentab(tabname){
 
 
 
-window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        alert("Success!");
-      })
-    });
-  });
+
   
      
