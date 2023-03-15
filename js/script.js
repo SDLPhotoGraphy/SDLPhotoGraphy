ded22d370f4932f017047a60788609b92d5a0643
SDLPhotoGraphy <122206595+SDLPhotoGraphy@users.noreply.github.com>
2023-03-15 20:01:38 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Sdl PhotoGraphy', 'Photographyer', 'VideoGraphyer' , 'Video Editor ',],
+    strings: ['Sdl PhotoGraphy', 'videographer', 'photographer' , 'Video Editor ',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -142,6 +142,8 @@ window.onload = ()=>{
 
     
 }
+
+
 
 
 
